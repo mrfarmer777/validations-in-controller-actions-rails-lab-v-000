f1f37e826aa16df493b96b275ae1d601d60db97d
Matt Farmer <mrfarmer777@gmail.com>
2018-07-11 16:00:19 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,8 +7,8 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create!(author_params)
     if @author.valid?
+      @author = Author.create!(author_params)
       redirect_to author_path(@author)
     else
       render :new
